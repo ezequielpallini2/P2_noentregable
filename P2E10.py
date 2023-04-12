ec8c00d64cccfaad97f21cbb650bce3351d29faa
Ezequiel Pallini <106202985+ezequielpallini2@users.noreply.github.com>
2023-04-12 00:40:39 -0300

    Falta comentar código

--- P2E10.py
+++ P2E10.py
@@ -14,14 +14,23 @@ notas_2 = [30, 95, 28, 84, 84, 43, 66, 51, 4, 11, 58, 10, 13, 34, 96, 71, 86, 37
 64, 13, 8, 87, 14, 14, 49, 27, 55, 69, 77, 59, 57, 40, 96, 24, 30, 73,
 95, 19, 47, 15, 31, 39, 15, 74, 33, 57, 10]
 
-#Creo una lista por comprensión, bueno mejor una tupla, indicando que los elementos a guardar en la lista sean
-#solo aquellos que contienen letras, para evitar guardar comas y comillas en la lista de nombres. primero probe con ascii_ letters 
-# pero tuve que buscar otra funcion para validar porque andrés no existía
+
+
+
+
 nombres_lista = [nombre.title() for nombre in nombres.strip().split("'") if all(letra.isalnum() for letra in nombre) and nombre != ""]
 #Creo un diccionario por comprensión guardando ambas notas de las alumnos en una lista, y a su vez asignando esa lista a el nombre 
 #utilizando el metodo enumerate()
 diccionario = {nombre : [notas_1[i], notas_2[i]] for i, nombre in enumerate(nombres_lista)}
 
 promedios = {nombre : ((notas_1[i] + notas_2[i]) / 2) for i, nombre in enumerate(nombres_lista)}
+print(promedios)
+promedio_general = sum(promedios.values()) / len(promedios)
 
-general = 
+
+clave_max, valor_max = max(promedios.items(), key=lambda x: x[1])
+print('El estudiante con mayor promedio es:', clave_max)
+
+clave_min, valor_min = min(diccionario.items(), key=lambda x: min(x[1]))
+
+print('El estudiante con menor nota es:', clave_min)
